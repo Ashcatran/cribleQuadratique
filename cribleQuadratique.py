b49beb1d356fb138b89dfb273f71632bca45daf2
Nicolas Donnaint <ashcatran@gmail.com>
2018-12-13 19:49:45 +0100

    added gcd function

--- cribleQuadratique.py
+++ cribleQuadratique.py
@@ -1,6 +1,11 @@
 #coding utf-8
 #!/usr/bin/env python3
 
+#https://www.utc.fr/~wschon/sr06/UtCrible/QuadraSievePage.php
+#http://pauillac.inria.fr/~maranget/X/IF/PI/maranget/sujet.html
+#https://pdfs.semanticscholar.org/14cb/d2f962f00cd7572e4e45f6c8ea7ce4fbcd81.pdf
+
+
 import math
 
 def primeNumbers(limit, primeList):
@@ -14,11 +19,24 @@ def isPrime(number, primeList):
             return False
     return True
 
+def gcd(a,b): #euclide's algorithm
+    if b == 0:
+        return a 
+    if a >= b:
+        return gcd(b, a % b) 
+    return gcd(b, a)
+
 if __name__ == "__main__":
+    """
     s = "hello world"
     print(s)
+    N = 403034474719 
+    B = 241 
+    L = 1850
     primeList = []
     primeNumbers(100, primeList)
     for item in primeList:
         print(item)
+    """
+    print(gcd(56, 6))
 
