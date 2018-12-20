c2a8c67fee4cf0335edb16cdf0f3267e89ebb154
Nicolas Donnaint <ashcatran@gmail.com>
2018-12-20 19:51:53 +0100

    Added comments and removed unnecessary code

--- cribleQuadratique.py
+++ cribleQuadratique.py
@@ -11,12 +11,15 @@ import pickle
 import random
 import itertools
 
+
 def primeNumbers(limit, primeList):
+    """returns primelist with all prime numbers from 2 to limit"""
     for i in range(1,limit):
         if isPrime(i, primeList):
             primeList.append(i)
 
 def getPrimeNumbers(start, end, primeList):
+    """adds all prime numbers between start and end into primelist"""
     number = end - start
     prevStatus = 0
     for i in range(start,end):
@@ -27,19 +30,20 @@ def getPrimeNumbers(start, end, primeList):
             print(status, "%")
 
 def isPrime(number, primeList):
+    """returns true is number is prime"""
     for i in primeList:
         if i != 1 and i != number and number % i == 0:
             return False
     return True
 
 def getPrimeNumbersUpTo(limit):
+    """returns a list of all prime numbers from 2 to limit. Loads and saves them from/into a file"""
     lastPrime = 2
     primeList = []
     try:
         primeList = pickle.load(open("primeList.p", "rb"))
         lastPrime = primeList[-1]
     except FileNotFoundError :
-    #as identifier:
         print("Creating new prime list")
     
     if lastPrime < limit:
@@ -55,6 +59,7 @@ def getPrimeNumbersUpTo(limit):
 
 
 def gcd(a,b): #euclide's algorithm
+    """return the greatest common divisor of a and b"""
     if b == 0:
         return a 
     if a >= b:
@@ -62,11 +67,13 @@ def gcd(a,b): #euclide's algorithm
     return gcd(b, a)
 
 def factorBaseSize(N):
+    """returns the optimal size of the factor base, knowing N"""
     F = pow(math.exp(math.sqrt(math.log(N)*math.log(math.log(N)))),math.sqrt(2)/4)
     F = int(math.ceil(F))
     return F
 
 def primeFactorization(N, primeList):
+    """returns the list of factors of N in primelist"""
     i=0
     facto = []
     if N < 0:
@@ -82,6 +89,7 @@ def primeFactorization(N, primeList):
     return facto
 
 def isSquare(number):
+    """returns True if number is a perfect square"""
     if number < 0:
         return False
     sqrt = math.sqrt(number)
@@ -89,19 +97,23 @@ def isSquare(number):
     return True if sqrt == floorSqrt else False
 
 def legendre(a, p):
+    """returns the legendre symbol of a and p"""
     return pow(a, (p - 1) // 2, p)
 
 def completelyFactores(q, S, e):
+    """returns True if q is completely factored by factors in S to the exponents in e"""
     n = 1
     for i in range(len(S)):
         n = n*(S[i]**e[i])
     return n == q
 
 def showMatrix(m):
+    """prints a matrix in a readable format"""
     for row in m:
         print(row)
 
 def makeBinaryVectors(matrix):
+    """returns a binary matrix"""
     newMatrix = []
     for row in matrix:
         newRow = []
@@ -111,7 +123,8 @@ def makeBinaryVectors(matrix):
     return newMatrix
 
 def findLinearCombination(matrix, allCombinations):
-    combination =[0 for i in range(len(matrix))]
+    """finds a linear combination of rows in matrix, using combinations in allCombinations"""
+    combination =[0 for i in range(len(matrix))]#indispensable?
     valid = False
     while valid == False:
         result = [0 for i in range(len(matrix[0]))]
@@ -127,85 +140,91 @@ def findLinearCombination(matrix, allCombinations):
                 valid = False
         if valid == False:
             removeCombination(allCombinations, combination)
-
-
     return combination
 
 def removeCombination(allCombinations, combination):
+    """removes combination form allCombinations"""
     allCombinations.remove(combination)
     if len(allCombinations) == 0:
         print("Tested all combination, none worked :(")
         exit()
 
+# looking for X**2 = X**2 mod N
 if __name__ == "__main__":
-    """
-    s = "hello world"
-    print(s)
-    """
     #N = 403034474719 
     #B = 241 
     #L = 1850
     #N = 24961 
     #N = 1511*1523
-    #N = 227*229
+    N = 227*229
     #N = 3119*3121
-    N = 2301351
+    #N = 2301351 # Doesn't work :(
     limit = N
     primeList = getPrimeNumbersUpTo(limit)
     print("loaded", len(primeList), "prime numbers, from", primeList[0], "to", primeList[-1])
+
+    """If N is prime, exit"""
     if isPrime(N, primeList):
         print(N, "is prime")
         exit(0)
+    
     m = math.floor(math.sqrt(N))
     f = factorBaseSize(N)
     # Création de la base de facteurs
     S = [-1]
     for p in primeList:
-        if legendre(N, p) == 1:
+        if legendre(N, p) == 1: # if p is a quadratic residue of N
             S.append(p)
         if len(S) == f:
             break
-            
+    
+    # calculating vectors and marix
     matrix = []
     aVector = []
     bVector = []
     for x in range(-f,f+1):
         q = (x + m)**2 - N
         a = x + m
-        e = primeFactorization(q, S[1:])
+        e = primeFactorization(q, S[1:])#ignore -1 in S
         cf = completelyFactores(q, S, e)
-        if cf:
+        if cf: # if q is S-smooth
             aVector.append(a)
             bVector.append(q)
             matrix.append(e)
+    
+    # constructing matrix
     binaryMatrix = makeBinaryVectors(matrix)
+
+    # creating all possible combinations
     allCombinations = list(itertools.product([0, 1], repeat = len(matrix)))
+
+    # solving loop
     solved = False
     while solved == False:
+        # obtain a valid combination of rows in matrix
         linearCombination = findLinearCombination(binaryMatrix, allCombinations)
+        # calculate X
         v = [0 for i in range(f)]
         for i in range(len(matrix)):
             for j in range(f):
+                # fectch exponents
                 v[j] = v[j] + matrix[i][j]*linearCombination[i]
         for i in range(len(v)):
-            v[i]=v[i]//2
+            v[i]=v[i]//2 # X is a perfect square
         v[0]=0
         X=1
         for i in range(f):
             X = X*S[i]**v[i]
         X = X%N
-        Z = 0
-        K=2
-        while (X**2)%N !=(Z**2)%N:
-            Z = (X - K*X)%N
-            K = K+1
 
+        # calculate Y
         Y = 1
         for i in range(len(matrix)):
             if linearCombination[i] > 0:
                 Y = Y*aVector[i]*linearCombination[i]
         Y = Y%N
 
+        # computing the two factors of N
         sol1 = gcd(abs(X-Y), N)
         sol2 = gcd(X+Y, N)
         if sol1 != 1 and sol2 != 1:
